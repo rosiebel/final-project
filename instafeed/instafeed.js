c9ba6464b1169f535b09562ccffc8120acd4edda
rosiebel <rosiebelpasso@Rosies-MacBook-Pro-2.local>
2014-11-24 21:11:21 -0800

    Instafeed works!
    Added 4 instafeeds to the instafeed.js

--- instafeed/instafeed.js
+++ instafeed/instafeed.js
@@ -1,7 +1,33 @@
 var feed = new Instafeed({
-        get: 'tagged',
-        tagName: 'awesome',
-        clientId: 'YOUR_CLIENT_ID',
-        template: '<a href="{{link}}"><img src="{{image}}" /></a>'
+       get: 'tagged',
+        tagName: 'naturebox',
+        clientId: '0c45d775c4d94644a55f79b09d0809b4',
+        template: '<a href="{{link}}"><img src="{{image}}" /></a>',
+        sortBy: 'most-liked',
+        resolution: 'thumbnail',
+        limit: 700
     });
-    feed.run();
+
+var feed2 = new Instafeed({
+       get: 'tagged',
+        tagName: 'snackselfie',
+        clientId: '0c45d775c4d94644a55f79b09d0809b4',
+        template: '<a href="{{link}}"><img src="{{image}}" /></a>',
+        sortBy: 'most-liked',
+        resolution: 'thumbnail',
+        limit: 700
+    });
+
+var feed3 = new Instafeed({
+       get: 'tagged',
+        tagName: 'Fall4NatureBox',
+        clientId: '0c45d775c4d94644a55f79b09d0809b4',
+        template: '<a href="{{link}}"><img src="{{image}}" /></a>',
+        sortBy: 'most-liked',
+        resolution: 'thumbnail',
+        limit: 700
+    });
+
+feed.run();
+feed2.run();
+feed3.run();
