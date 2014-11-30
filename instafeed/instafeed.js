5941146ee7fc04e2bc967a1de16350cf41549d65
rosiebel <rosiebelpasso@rosies-mbp-2.attlocal.net>
2014-11-30 15:38:24 -0800

    Removed 'media' div

--- instafeed/instafeed.js
+++ instafeed/instafeed.js
@@ -28,6 +28,18 @@ var feed3 = new Instafeed({
         limit: 700
     });
 
+var feed4 = new Instafeed({
+       get: 'tagged',
+        tagName: 'Nature Box',
+        clientId: '0c45d775c4d94644a55f79b09d0809b4',
+        template: '<a href="{{link}}"><img src="{{image}}" /></a>',
+        sortBy: 'most-liked',
+        resolution: 'thumbnail',
+        limit: 700
+    });
+
+
 feed.run();
 feed2.run();
 feed3.run();
+feed4.run();
